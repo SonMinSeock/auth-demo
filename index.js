3d5cb34e430bad3cf40765d230cd604daccd9c27
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-08-02 16:12:28 +0900

    Auth Demo 회원가입 기능구현 bcrypt 이용

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
+// module
 const express = require("express");
 const User = require("./models/user");
 const path = require("path");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const PORT = 3000;
+
+// MongoDB 연결 작업
+mongoose
+  .connect("mongodb://127.0.0.1:27017/loginDemo")
+  .then(() => console.log("Mongo Connection Open ✅"))
+  .catch((err) => console.log("Mongo connecting Error : ", err));
 
 // setting
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+//middleware
+app.use(express.urlencoded({ extended: true }));
+
 // router
+app.get("/", (req, res) => {
+  res.send("This is the Home Screen");
+});
 app.get("/register", (req, res) => {
   res.render("register");
+});
+app.post("/register", async (req, res) => {
+  const {
+    body: { username, password },
+  } = req;
+
+  // make hash
+  const hash = await bcrypt.hash(password, 12);
+
+  // DB Make User
+  const user = new User({ username, password: hash });
+
+  await user.save();
+  console.log(user);
+  res.redirect("/");
 });
 app.get("/secret", (req, res) => {
   res.send(`This is Secret! You Cannot See Me`);
